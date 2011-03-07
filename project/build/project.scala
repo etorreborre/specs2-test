f8c7f15baaac29954eadb7f2ba71e974f6ab0c89
etorreborre <etorreborre@yahoo.com>
2011-03-08 07:28:07 +1100

    upgraded pegdown

--- project/build/project.scala
+++ project/build/project.scala
@@ -16,14 +16,12 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val hamcrest      = "org.hamcrest" % "hamcrest-all" % "1.1"
   val mockito 	 	= "org.mockito" % "mockito-all" % "1.8.5" 
   val junit 		= "junit" % "junit" % "4.7"
-  val parboiled     = "org.parboiled" % "parboiled4j" % "0.9.9.0"
-  val pegdown       = "org.pegdown" % "pegdown" % "0.8.5.4"
+  val pegdown       = "org.pegdown" % "pegdown" % "0.9.0"
   val specs2        = "org.specs2" %% "specs2" % "1.0"
   
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
 
   val snapshotsRepo = "snapshots-repo" at "http://scala-tools.org/repo-snapshots"
-  val specsRepo = "specs-repo" at "http://specs.googlecode.com/svn/maven2"
 
 }
