a576222142a9a2ec6cb180a08f6bf75e615063ff
etorreborre <etorreborre@yahoo.com>
2011-04-29 23:35:35 +1000

    updated the dependencies in the test project

--- project/build/project.scala
+++ project/build/project.scala
@@ -12,12 +12,11 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
 
   val scalacheck 	  = "org.scala-tools.testing" %% "scalacheck" % "1.8" 
   val testinterface = "org.scala-tools.testing" % "test-interface" % "0.5" 
-  val scalazcore 	  = "com.googlecode.scalaz" %% "scalaz-core" % "5.1-SNAPSHOT"
   val hamcrest      = "org.hamcrest" % "hamcrest-all" % "1.1"
   val mockito 	 	  = "org.mockito" % "mockito-all" % "1.8.5" 
   val junit 		    = "junit" % "junit" % "4.7"
   val pegdown       = "org.pegdown" % "pegdown" % "0.9.1"
-  val specs2        = "org.specs2" %% "specs2" % "1.1"
+  val specs2        = "org.specs2" %% "specs2" % "1.2"
   
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
