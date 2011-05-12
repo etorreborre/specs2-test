82c4a490db56b1d3596ead71e89fe8ff21b77744
etorreborre <etorreborre@yahoo.com>
2011-05-13 08:29:56 +1000

    released 1.3 for scala 2.9.0

--- project/build/project.scala
+++ project/build/project.scala
@@ -8,7 +8,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   override def compileOptions = Unchecked :: super.compileOptions.toList
   override def javaCompileOptions = JavaCompileOption("-Xmx256m -Xms64m -Xss1M") :: Nil
   override def testJavaCompileOptions = JavaCompileOption("-Xmx256m -Xms64m") :: Nil
-  override def includeTest(s: String) = { s.endsWith("Spec")  }
+  override def includeTest(s: String) = { s.endsWith("Spec") || s == "index" }
 
   val scalacheck 	  = "org.scala-tools.testing" %% "scalacheck" % "1.8" 
   val testinterface = "org.scala-tools.testing" % "test-interface" % "0.5" 
@@ -16,8 +16,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val mockito 	 	  = "org.mockito" % "mockito-all" % "1.8.5" 
   val junit 		    = "junit" % "junit" % "4.7"
   val pegdown       = "org.pegdown" % "pegdown" % "0.9.1"
-  val specs2        = "org.specs2" %% "specs2" % "1.3-SNAPSHOT"
-  val scalazcore    = "org.specs2" %% "scalaz-core" % "5.1-SNAPSHOT" 
+  val specs2        = "org.specs2" %% "specs2" % "1.3"
+  val scalazcore    = "org.specs2" %% "scalaz-core" % "6.0.RC2" 
   
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
